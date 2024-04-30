8941bae945606103321952fe75770dcd5ac6766c
hichem <hichemlamine@gmail.com>
2024-04-30 09:52:16 +0200

    Update URL_DATABASE in databse.py

--- fastapi+postgres/app/database.py
+++ fastapi+postgres/app/database.py
@@ -3,11 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-# URL_DATABASE = 'postgresql://postgres:postgres@localhost:5432/quiz'
+URL_DATABASE = 'postgresql://postgres:postgres@localhost:5432/quiz'
 # URL_DATABASE = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/quiz')
 # URL_DATABASE = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@postgres-svc:5432/quiz')
-URL_DATABASE = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@postgres-svc.default.svc.cluster.local:5432/quiz')
-
+# URL_DATABASE = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@postgres-svc.default.svc.cluster.local:5432/quiz')
 # URL_DATABASE = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@postgres-svc.default.svc.kubernetes.local:5432/quiz')
 
 
